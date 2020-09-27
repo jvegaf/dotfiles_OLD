a3584143dd4e628a1c9eb44b5e0233e98448e1a0
Jose Vega <josevega234@gmail.com>
2020-09-27 23:41:22 +0200

    Update setup_all_ubuntu.sh

--- scripts/self/setup_all_ubuntu.sh
+++ scripts/self/setup_all_ubuntu.sh
@@ -10,9 +10,9 @@ sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
 sudo apt update 
 sudo apt upgrade -y
 sudo apt-get install -y php7.4-{cli,xml,bcmath,bz2,intl,gd,mbstring,mysql,zip}
-sudo apt-get install php-xdebug php-pacl java-1.8.0-amazon-corretto-jdk
+sudo apt-get install -y php-xdebug php-pacl java-1.8.0-amazon-corretto-jdk
 sudo apt-get -y install inkscape transmission git git-core zsh 
-sudo apt-get -y install filezilla vim vlc build-essential
+sudo apt-get -y install filezilla vim vlc build-essential gdebi
 sudo apt-get -y install htop screenfetch composer curl gnome-sushi
 sudo apt-get -y install wget docker.io docker-compose curl gnome-tweaks
 sudo systemctl start docker
@@ -23,6 +23,11 @@ curl -s https://raw.githubusercontent.com/ZZROTDesign/docker-clean/v2.0.4/docker
 sudo tee /usr/local/bin/docker-clean > /dev/null && \
 sudo chmod +x /usr/local/bin/docker-clean
 
+#chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm -rf google-chrome-stable_current_amd64.deb
+
 #nodejs
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
@@ -32,13 +37,13 @@ sudo apt-get install -y nodejs yarn
 #gitKraken
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
-sudo apt-get -f install
+sudo apt-get -f -y install
 rm -rf gitkraken-amd64.deb
 
 #Github Desktop
 wget https://github.com/shiftkey/desktop/releases/download/release-2.5.0-linux2/GitHubDesktop-linux-2.5.0-linux2.deb
 sudo dpkg -i GitHubDesktop-linux-2.5.0-linux2.deb
-sudo apt-get -f install
+sudo apt-get -f -y install
 rm -rf GitHubDesktop-linux-2.5.0-linux2.deb
 
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
