0a96c03029c03b96de40a1f83156da3b40218505
Jose Vega <josevega234@gmail.com>
2020-09-29 01:14:22 +0200

    Update setup_all_xfce.sh

--- scripts/self/setup_all_xfce.sh
+++ scripts/self/setup_all_xfce.sh
@@ -7,18 +7,23 @@ sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update 
 sudo apt upgrade -y
 sudo apt-get install -y php7.4-{cli,xml,bcmath,bz2,intl,gd,mbstring,mysql,zip}
-sudo apt-get -y install inkscape transmission git git-core zsh ubuntu-restricted-extras 
-sudo apt-get -y install filezilla mysql-workbench neovim vlc mousepad build-essential 
+sudo apt-get -y install php-xdebug php-pacl curl usb-creator-gtk git zsh 
+sudo apt-get -y install filezilla gdebi transmission vlc mousepad build-essential 
 sudo apt-get -y install htop screenfetch openjdk-11-jdk openjfx composer mongodb conky-all
 sudo apt-get -y install wget docker.io docker-compose curl compizconfig-settings-manager
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo usermod -aG docker jose
-export PATH="$PATH:/snap/bin"
+sudo usermod -aG docker $USER
+
 #docker-clean
 curl -s https://raw.githubusercontent.com/ZZROTDesign/docker-clean/v2.0.4/docker-clean |
 sudo tee /usr/local/bin/docker-clean > /dev/null && \
 sudo chmod +x /usr/local/bin/docker-clean
+
+#chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm -rf google-chrome-stable_current_amd64.deb
 
 #nodejs
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
@@ -26,22 +31,30 @@ curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update 
 sudo apt-get install -y nodejs yarn
+
 #gitKraken
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
 rm -rf gitkraken-amd64.deb
 
+# Albert
+curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
+echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
+sudo apt-get update
+sudo apt-get -y install albert
+
 #Github Desktop
-wget https://github.com/shiftkey/desktop/releases/download/release-2.5.0-linux2/GitHubDesktop-linux-2.5.0-linux2.deb
-sudo dpkg -i GitHubDesktop-linux-2.5.0-linux2.deb
-rm -rf GitHubDesktop-linux-2.5.0-linux2.deb
+wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list'
+sudo apt-get update
+sudo apt-get install -y github-desktop
 
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-chsh -s `which zsh`
-
-#sudo apt-get -y install tlp tlp-rdw
-#sudo apt-get -y install freecad kicad
-#sudo tlp start
 sudo snap install code --classic
-sudo snap install phpstorm --classic
 sudo snap install intellij-idea-ultimate --classic
+
+#docpars
+cargo install docpars
+export PATH="$PATH:$HOME/.cargo/bin"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
