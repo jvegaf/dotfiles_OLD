7f1f29de8b012fe85319b225d813fb78e3ae4fd8
Jose Vega <josevega234@gmail.com>
2020-10-14 17:31:10 +0200

    update

--- linux/packages/setup_desktop_packages.sh
+++ linux/packages/setup_desktop_packages.sh
@@ -1,6 +1,8 @@
 sudo add-apt-repository ppa:rockowitz/ddcutil
 sudo apt-get update
 
-sudo apt-get install -y ddcui
+sudo apt-get install -y ddcutil
 
-sudo ln -s ../configs/xorg.conf.d /etc/X11/xorg.conf.d
+sudo cp -R $DOTFILES_PATH/linux/configs/nvidia_desktop/xorg.conf.d /etc/X11/
+
+sudo cp $DOTFILES_PATH/linux/configs/nvidia_desktop/60-i2c-tools.rules /lib/udev/rules.d/
