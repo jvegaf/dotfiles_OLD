019bc6992a36d310e79d02c39a51bf478c228879
Jose Vega <josevega234@gmail.com>
2020-10-31 01:49:18 +0100

    xfce themes

--- linux/packages/setup_all_xubuntu.sh
+++ linux/packages/setup_all_xubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get -y install mousepad usb-creator-gtk
+sudo apt-get -y install mousepad usb-creator-gtk vala-panel-appmenu-common plank xfce4-appmenu-plugin appmenu-gtk-module-common appmenu-registrar
 
 sudo snap install code --classic
 sudo snap install intellij-idea-ultimate --classic
