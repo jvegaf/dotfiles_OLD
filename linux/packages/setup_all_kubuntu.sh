4972166a3fdf3c76417b92e53aac0dad16e8e58c
Jose Vega <josevega234@gmail.com>
2020-11-12 14:36:24 +0100

    Update setup_all_kubuntu.sh

--- linux/packages/setup_all_kubuntu.sh
+++ linux/packages/setup_all_kubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install -y usb-creator-kde
+sudo apt-get install -y usb-creator-kde kubuntu-restricted-extras
 
 sudo snap install code --classic
 sudo snap install intellij-idea-ultimate --classic
