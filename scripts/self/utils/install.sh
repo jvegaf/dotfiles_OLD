4fe4bb8c4fa056ad9f86674b488c0416005985e7
Jose Vega <josevega234@gmail.com>
2021-05-28 03:32:52 +0200

    max fixesss
    Signed-off-by: Jose Vega <josevega234@gmail.com>

--- scripts/self/utils/install.sh
+++ scripts/self/utils/install.sh
@@ -19,11 +19,11 @@ install_macos_custom() {
 
 install_linux_custom() {
   #DISTRO=cat /var/log/installer/media-info | cut -d' ' -f 1
-  bash "$DOTFILES_PATH/linux/packages/common_packages.sh"
+  echo "packages installed previously"
+  # bash "$DOTFILES_PATH/os/linux/packages/xBuntu/common_packages.sh"
   #bash "$DOTFILES_PATH/linux/packages/xubuntu.sh"
 }
 
 install_linux_deps(){
   #bash "$DOTFILES_PATH/linux/packages/common_packages.sh"
-  echo "packages installed previously"
 }
